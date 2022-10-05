24537b538b599409e20276297c5ae975ae324fa6
Eryk Juszczak <juszczakeryk8@gmail.com>
2022-10-05 22:43:49 +0200

    Changes to form i used map to reduce need to code and remain dry

--- src/Components/Ingredients/IngredientForm/IngredientForm.jsx
+++ src/Components/Ingredients/IngredientForm/IngredientForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useFormik } from 'formik'
+import { Formik, useFormik } from 'formik'
 import { TextField, Button } from '@mui/material'
 import './IgredientForm.scss'
 const IngredientForm = () => {
@@ -44,78 +44,37 @@ const IngredientForm = () => {
 			console.log(values)
 		},
 	})
+	const data = [
+		{ id: 'ingredientName', label: 'Nazwa Produktu', variant: 'standard', type: 'text' },
+		{ id: 'ingredientCarbs', label: 'Węglowodany', variant: 'standard', type: 'number' },
+		{ id: 'ingredientProtein', label: 'Białko', variant: 'standard', type: 'number' },
+		{ id: 'ingredientPotasium', label: 'Potas', variant: 'standard', type: 'number' },
+		{ id: 'ingredientSodium', label: 'Sód', variant: 'standard', type: 'number' },
+		{ id: 'ingredientFat', label: 'Tłuszcz', variant: 'standard', type: 'number' },
+		{ id: 'ingredientKcal', label: 'Kcal', variant: 'standard', type: 'number' },
+	]
 	return (
 		<form onSubmit={ingredientForm.handleSubmit} className='form'>
 			<div className='input-container'>
-				<TextField
-					id='ingredientName'
-					name='ingredientName'
-					label='Nazwa Produktu'
-					variant='standard'
-					type='text'
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientName}
-				/>
-				<TextField
-					id='ingredientCarbs'
-					variant='standard'
-					label='Węglowodany'
-					name='ingredientCarbs'
-					type='number'
-					onBlur={ingredientForm.handleBlur}
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientCarbs}
-				/>
-				<TextField
-					id='ingredientFat'
-					name='ingredientFat'
-					label='Tłuszcz'
-					variant='standard'
-					type='number'
-					onBlur={ingredientForm.handleBlur}
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientFat}
-				/>
-				<TextField
-					id='ingredientProtein'
-					variant='standard'
-					label='Białko'
-					name='ingredientProtein'
-					type='number'
-					onBlur={ingredientForm.handleBlur}
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientProtein}
-				/>
-				<TextField
-					id='ingredientSodium'
-					variant='standard'
-					label='Sód'
-					name='ingredientSodium'
-					type='number'
-					onBlur={ingredientForm.handleBlur}
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientSodium}
-				/>
-				<TextField
-					id='ingredientPotasium'
-					variant='standard'
-					label='Potas'
-					name='ingredientPotasium'
-					type='number'
-					onBlur={ingredientForm.handleBlur}
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientPotasium}
-				/>
-				<TextField
-					id='ingredientKcal'
-					variant='standard'
-					label='Kalorie wyrażone w Kcal'
-					name='ingredientKcal'
-					type='number'
-					onBlur={ingredientForm.handleBlur}
-					onChange={ingredientForm.handleChange}
-					value={ingredientForm.values.ingredientKcal}
-				/>
+				{data.map(e => {
+					return (
+						<>
+							<TextField
+								key={e.id}
+								id={e.id}
+								name={e.id}
+								label={e.label}
+								type={e.type}
+								variant={e.variant}
+								onChange={ingredientForm.handleChange}
+								onBlur={ingredientForm.handleBlur}
+							/>
+							{ingredientForm.touched && ingredientForm.errors ? (
+								<div>{ingredientForm.errors.id}</div>
+							) : null}
+						</>
+					)
+				})}
 			</div>
 			<Button className='form-button' variant='outlined' type='submit'>
 				Dodaj
